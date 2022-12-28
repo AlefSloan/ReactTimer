36329e74f8493ab3d252203f7495f9252332c96b
Alef Sloan <alef_sloan@hotmail.com>
2022-12-28 20:57:56 -0300

    feat: import Router in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './Router'
+
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global/global'
@@ -5,8 +9,9 @@ import { GlobalStyle } from './styles/global/global'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>HelloWorld</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
